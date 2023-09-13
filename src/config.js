ec0138aed1b6c3d5e33983a931f6b50255d4cf0e
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-09-13 07:41:17 -0600

    DB connection changes

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ dotenv();
 export const config = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER  || 'root', 
-    password: process.env.DB_PASSWORD || '',
+    /*password: process.env.DB_PASSWORD || 'jsbsl)3n49)2!#k',*/
+    password: 'jsbsl)3n49)2!#k',
     database: process.env.DB_DATABASE || 'database',
 } 
--- src/database.js
+++ src/database.js
@@ -2,6 +2,6 @@ import mysql from 'mysql2/promise';
 import { config } from './config'
 
 export const connect = async () => {
-    return await mysql.createConnection(config);
+    return mysql.createConnection(config);
 }
  
